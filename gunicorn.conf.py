4426d54f2512218e8d5e6314259800a6cac51cb0
Victor Miti <victormiti@umusebo.com>
2025-05-10 00:34:23 +0100

    fix: Set Gunicorn to bind to 0.0.0.0 to allow external connections

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,7 +13,7 @@ max_requests_jitter = 50
 # Log to stdout
 accesslog = "-"
 loglevel = "info"
-# Use X-Forwarded-For header to get real client IP instead of Nginx proxy IP
+# Use X-Forwarded-For header to get real client IP instead of reverse proxy IP
 access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 # Workers can be overridden by `$WEB_CONCURRENCY`
@@ -21,3 +21,7 @@ workers = 3
 
 # Load app pre-fork to save memory and worker startup time
 preload_app = True
+
+# Bind to all network interfaces so Traefik can connect
+# https://docs.gunicorn.org/en/stable/settings.html#bind
+bind = "0.0.0.0:8000"
